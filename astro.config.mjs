980cda0ba95449cd5db403853608b79c11d83606
summer_fi2021 <summer_fi2021@163.com>
2023-02-23 22:30:54 +0800

    feat: unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import unoCss from '@unocss/astro';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import vue from "@astrojs/vue";
@@ -8,5 +9,23 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), vue()]
+  integrations: [
+    mdx(), 
+    sitemap(),
+    vue(),
+    unoCss({
+      presets: []
+    })
+  ],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'entry.[hash].js',
+          chunkFileNames: 'chunks/chunk.[hash].js',
+          assetFileNames: 'assets/asset.[hash][extname]',
+        },
+      },
+    }
+  }
 });
